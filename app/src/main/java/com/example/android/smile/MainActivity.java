94e6c5da22c1e07c92c1117f5702fe197475d437
JSbhagiya <bhagiyajanvi@gmail.com>
2018-08-19 12:09:21 +0530

    New Update

--- app/src/main/java/com/example/android/smile/MainActivity.java
+++ app/src/main/java/com/example/android/smile/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.smile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView numbers=(TextView)findViewById(R.id.numbers);
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Numbers.class));
+            }
+        });
+
+        TextView alphabets=(TextView)findViewById(R.id.alphabet);
+        alphabets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Alphabet.class));
+            }
+        });
+        TextView phrases=(TextView)findViewById(R.id.phrases);
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Phrases.class));
+            }
+        });
+        TextView rhymes=(TextView)findViewById(R.id.rhymes);
+        rhymes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Rhymes.class));
+            }
+        });
     }
 }
