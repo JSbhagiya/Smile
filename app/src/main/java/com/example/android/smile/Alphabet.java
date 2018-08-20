0486d20050dc0bba3e15be9c93a61e4fc1585ac9
JSbhagiya <bhagiyajanvi@gmail.com>
2018-08-20 18:59:10 +0530

    New Updates

--- app/src/main/java/com/example/android/smile/Alphabet.java
+++ app/src/main/java/com/example/android/smile/Alphabet.java
@@ -27,6 +27,23 @@ public class Alphabet extends AppCompatActivity {
         alphabet.add(new word("H",R.drawable.h));
         alphabet.add(new word("I",R.drawable.i));
         alphabet.add(new word("J",R.drawable.j));
+        alphabet.add(new word("K",R.drawable.k));
+        alphabet.add(new word("L",R.drawable.l));
+        alphabet.add(new word("M",R.drawable.m));
+        alphabet.add(new word("N",R.drawable.n));
+        alphabet.add(new word("O",R.drawable.o));
+        alphabet.add(new word("P",R.drawable.p));
+        alphabet.add(new word("Q",R.drawable.q));
+        alphabet.add(new word("R",R.drawable.r));
+        alphabet.add(new word("S",R.drawable.s));
+        alphabet.add(new word("T",R.drawable.t));
+        alphabet.add(new word("U",R.drawable.u));
+        alphabet.add(new word("V",R.drawable.v));
+        alphabet.add(new word("W",R.drawable.w));
+        alphabet.add(new word("X",R.drawable.x));
+        alphabet.add(new word("Y",R.drawable.y));
+        alphabet.add(new word("Z",R.drawable.z));
+
 
         GridView list= (GridView) findViewById(R.id.gridview);
         wordAdapter adapter = new wordAdapter(this,alphabet);
